fc37e70049b1b09c0031fbe9022006cc1f80d60e
Rajat Kanthhaliya <rkanthaliya@deloitte.com>
2019-03-06 16:55:52 +0530

    minor CSS changes

--- src/component/shoppingCart.js
+++ src/component/shoppingCart.js
@@ -6,7 +6,7 @@ const ShoppingCart = (props) => {
             <p>Shopping Cart</p>
 
             {props.products.length>0 ? props.products.map((product, i) => (
-                <div key={i} className="product-card-container border-gray rounded border mx-1 my-1  bg-light">
+                <div key={i} className="cart-container border-gray rounded border mx-1 my-1  bg-light">
                     <div className="cart-value">
                         <span className="name">{product.productName}</span>
                         <button onClick={() => props.removeToCart(product.productName)} type="button" className="btn btn-secondary remove-cart">Remove</button>
